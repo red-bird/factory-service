0a8d7fdadcb54486e2ffd4ccd18871dd4aed4660
redbird <redbird8941@gmail.com>
2021-03-16 18:01:05 +0300

    factory-service initialized

---
+++ src/main/java/com/redbird/factoryservice/FactoryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.redbird.factoryservice;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@OpenAPIDefinition
+@EnableEurekaClient
+public class FactoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryServiceApplication.class, args);
+	}
+
+}
